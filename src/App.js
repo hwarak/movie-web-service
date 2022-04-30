07369751091db4876c6d32d7517d6777a457fba7
minhee <hwarakhada@gmail.com>
2022-05-01 08:39:31 +0900

    특정 state가 변할때마누 useEffect 사용하기

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   console.log("i run all the time");
   useEffect(() => {
     console.log("Call The API....");
   }, []);
+  useEffect(() => {
+    console.log("search for", keyword);
+  }, [keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search hear.."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click Me</button>
     </div>
