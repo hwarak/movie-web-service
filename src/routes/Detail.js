b23b627846a3c23e2c44f5fece479b5ec240f6d0
minhee <hwarakhada@gmail.com>
2022-05-01 22:54:26 +0900

    받은 아이디값으로디테일 페이지에 영화 정보 출력

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,33 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 function Detail() {
-  const [movie, setMovie] = useState();
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     setMovie(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
 
-  return <h1>{movie}</h1>;
+  return loading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div>
+      <img src={movie.medium_cover_image} alt="" />
+      <h1>{movie.title}</h1>
+      <p>{movie.description_full}</p>
+      <ul>
+        {movie.genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default Detail;
